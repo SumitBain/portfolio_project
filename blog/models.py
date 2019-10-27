a1acfda1290048995b1a91780eff0a3e7f698f9d
Sumit Bain <sumitbain123@gmail.com>
2019-10-28 02:59:44 +0530

    added bootstrap job

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Create your blog models
+#title
+#pub date
+#body
+#images
+class Blog(models.Model):
+    title= models.CharField(max_length=500)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
+
+# add the blog app to the setting
+
+
+#create a migration
+
+
+#migrate
+
+
+
+#add to the admin
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
-]
+    'blog.apps.BlogConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
