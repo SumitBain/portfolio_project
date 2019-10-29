981c64749522cccbd11d8d4846a39d1258c396aa
Sumit Bain <sumitbain123@gmail.com>
2019-10-29 20:06:17 +0530

    blog functionality and polish

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,8 +122,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'portfolio/static/'),
+     os.path.join(BASE_DIR, 'blog/static/')
+
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
